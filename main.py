835a3b92edb8dc35c127a866966dee880b04c727
James Zhang <jameszhanga@gmail.com>
2021-06-22 00:28:44 -0500

    Updates all paths to absolute to make crontab work

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ from bs4 import BeautifulSoup
 from datetime import date, datetime
 import requests
 import csv
+import os
 
-# Goes to the url and makes a request object
-URL = 'https://services.recwell.wisc.edu/FacilityOccupancy'
-source = requests.get(URL).text
+def log_occupancy():
+    # Goes to the url and makes a request object
+    URL = 'https://services.recwell.wisc.edu/FacilityOccupancy'
+    source = requests.get(URL).text
 
-# Retrives the data from the source
-soup = BeautifulSoup(source, 'html.parser')
-data = soup.find(attrs='occupancy-count').strong.text
+    # Retrives the data from the source
+    soup = BeautifulSoup(source, 'html.parser')
+    data = soup.find(attrs='occupancy-count').strong.text
 
-# Gets the date and time
-today = date.today()
-now = datetime.now()
-d = today.strftime('%m/%d/%y')
-t = now.strftime('%H:%M')
+    # Gets the date and time
+    today = date.today()
+    now = datetime.now()
+    d = today.strftime('%m/%d/%y')
+    t = now.strftime('%H:%M')
 
-# Writes to csv
-with open('data.csv', 'a') as file:
-    csv_f = csv.writer(file)
-    csv_f.writerow([d, t, data])
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, 'data.csv')
+
+    # Writes to csv
+    with open(file_path, 'a') as file:
+        csv_f = csv.writer(file)
+        csv_f.writerow([d, t, data])
+
+if __name__ == '__main__':
+    log_occupancy()
